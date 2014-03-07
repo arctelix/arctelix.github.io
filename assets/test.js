a9b7516922a60f1c72bd3a833b897f2493858d5e
arctelix <arctelix@gmail.com>
2014-03-07 00:27:08 -0500

    update  test.js

--- assets/test.js
+++ assets/test.js
@@ -1,34 +1,31 @@
-define(function (){
-    var outPut = document.getElementById('out-put');
-    return {
-        test: function (testString, assert, source){
+define({
+    test: function (testString, assert, source){
 
-            source = source || '';
-            var stringV;
+        source = source || '';
+        var stringV;
+        console.warn('test this', this.name)
+        try{
+            var value = eval(testString)
+            }catch(e){
+                var error = e.message.replace('<','').replace('>','');
+                var value = 'error'
+                }
 
-            try{
-                var value = eval(testString)
-                }catch(e){
-                    var error = e.message.replace('<','').replace('>','');
-                    var value = 'error'
-                    }
-
-            console.log(typeof value, ' = ', assert);
-            if (value === assert || typeof value == assert) {
-                assert = 'pass';
-                if (value=='error') value = error;
-                stringV = '<i class="pass">'+value+'</i>'
-            }else{
-                assert = 'fail';
-                if (value=='error') value = error;
-                stringV = '<i class="fail">'+value+'</i>'
-            }
-            //output
-            if (source) source = source + '.';
-            var outString = source + testString + ' = ';
-            if (assert == 'fail') console.error(assert, outString, value);
-            else console.log(assert, outString, value);
-            outPut.innerHTML += outString+' '+stringV+'<br>';
+        console.log(typeof value, ' = ', assert);
+        if (value === assert || typeof value == assert) {
+            assert = 'pass';
+            if (value=='error') value = error;
+            stringV = '<i class="pass">'+value+'</i>'
+        }else{
+            assert = 'fail';
+            if (value=='error') value = error;
+            stringV = '<i class="fail">'+value+'</i>'
         }
+        //output
+        if (source) source = source + '.';
+        var outString = source + testString + ' = ';
+        if (assert == 'fail') console.error(assert, outString, value);
+        else console.log(assert, outString, value);
+        return outString+' '+stringV+'<br>';
     }
 });
