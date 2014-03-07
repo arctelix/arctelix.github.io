acdc66f3cc1ba833c037a3f7637b43de593753a8
arctelix <arctelix@gmail.com>
2014-03-06 23:39:33 -0500

    update klass, require-config.js, test.js

--- assets/test.js
+++ assets/test.js
@@ -1,34 +1,36 @@
 define(function (){
 
     var outPut = document.getElementById('out-put');
-    test: function (testString, assert, source){
+    return {
+        test: function (testString, assert, source){
 
-        source = source || '';
-        var stringV;
+            source = source || '';
+            var stringV;
 
-        try{
-            var value = eval(testString)
-            }catch(e){
-                var error = e.message.replace('<','').replace('>','');
-                var value = 'error'
-                }
+            try{
+                var value = eval(testString)
+                }catch(e){
+                    var error = e.message.replace('<','').replace('>','');
+                    var value = 'error'
+                    }
 
-        console.log(typeof value, ' = ', assert);
-        if (value === assert || typeof value == assert) {
-            assert = 'pass';
-            if (value=='error') value = error;
-            stringV = '<i class="pass">'+value+'</i>'
-        }else{
-            assert = 'fail';
-            if (value=='error') value = error;
-            stringV = '<i class="fail">'+value+'</i>'
+            console.log(typeof value, ' = ', assert);
+            if (value === assert || typeof value == assert) {
+                assert = 'pass';
+                if (value=='error') value = error;
+                stringV = '<i class="pass">'+value+'</i>'
+            }else{
+                assert = 'fail';
+                if (value=='error') value = error;
+                stringV = '<i class="fail">'+value+'</i>'
+            }
+            //output
+            if (source) source = source + '.';
+            var outString = source + testString + ' = ';
+            if (assert == 'fail') console.error(assert, outString, value);
+            else console.log(assert, outString, value);
+            outPut.innerHTML += outString+' '+stringV+'<br>';
         }
-        //output
-        if (source) source = source + '.';
-        var outString = source + testString + ' = ';
-        if (assert == 'fail') console.error(assert, outString, value);
-        else console.log(assert, outString, value);
-        outPut.innerHTML += outString+' '+stringV+'<br>';
     }
 
 });
