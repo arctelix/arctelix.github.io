d3daec0d62e03bfb7ea7986f9e59cceccdb82412
arctelix <arctelix@gmail.com>
2014-03-07 02:56:03 -0500

    update  test.js

--- assets/test.js
+++ assets/test.js
@@ -1,5 +1,5 @@
 define(function(){
-    var  test = function (testString, assert, source){
+    var  test1 = function (testString, assert, source){
 
         source = source || '';
         var stringV;
@@ -34,5 +34,44 @@ define(function(){
         }
         return outString+' '+stringV+'<br>';
     }
-    return{test:test}
+    var test2 = function (obj, testName, assert, source){
+        if (obj){
+            var parts = testName.split('.')
+            parts[0] = 'obj'
+            testString = parts.join('.')
+        }
+        source = source || '';
+        var stringV;
+        try{
+            var value = eval(testString)
+        }catch(e){
+            var error = e.message.replace('<','').replace('>','');
+            var value = 'error'
+        }
+
+        console.log(typeof value, ' = ', assert);
+        if (value === assert || typeof value == assert) {
+            assert = 'pass';
+            if (value=='error') value = error;
+            stringV = '<i class="pass">'+value+'</i>'
+        }else{
+            assert = 'fail';
+            if (value=='error') value = error;
+            stringV = '<i class="fail">'+value+'</i>'
+        }
+        //output
+        if (source) source = source + '.';
+        var outString = source + testName + ' = ';
+        if (assert == 'fail') console.error(assert, outString, value);
+        else console.log(assert, outString, value);
+        try{
+            var outPut = document.getElementById('test-data')
+            outPut.innerHTML += outString+' '+stringV+'<br>';
+        }catch(e){
+
+        }
+        return outString+' '+stringV+'<br>';
+    }
+
+    return{test1:test1, test2:test2}
 });
